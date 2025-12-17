c62f4f1116f2a643ef31751ce3e828708b4acd5b
williambrioli <brioli@outlook.com>
2025-12-17 17:28:02 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -154,7 +154,8 @@ btnComprar.addEventListener("click", () => {
     `Categoria: ${slugParaNomeCategoria(produto.categoria)}%0A` +
     `Quantidade: ${quantidade} unidade(s)%0A` +
     `Valor unitário: ${formatarPreco(valorUnitario)}%0A` +
-    `Valor total: ${formatarPreco(valorTotal)}`;
+    `Valor total: ${formatarPreco(valorTotal)}%0A%0A` +
+    `Link do produto:%0A${linkProduto}`;
 
   // 3️⃣ abre o WhatsApp
   const url = `https://wa.me/${WHATSAPP_NUMERO}?text=${mensagem}`;
