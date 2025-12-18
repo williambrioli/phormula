5f309e34edf045419921c03bd87ccf5e2201b273
williambrioli <brioli@outlook.com>
2025-12-18 00:47:09 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -11,29 +11,29 @@ const WHATSAPP_NUMERO = "5518988092571";
 
 const categorias = [
   {
-    id: "analgesicos",
-    nome: "Analgésicos",
-    mostrarNoMenu: false
-  },
-  {
-    id: "cosmeticos",
-    nome: "Cosméticos",
+    id: "emagrecimento",
+    nome: "Emagrecimento",
     mostrarNoMenu: true
   },
   {
-    id: "vitaminas",
-    nome: "Vitaminas",
+    id: "desempenhofisico",
+    nome: "Desempenho Físico",
     mostrarNoMenu: true
   },
   {
-    id: "higiene",
-    nome: "Higiene",
+    id: "saude",
+    nome: "Saúde",
+    mostrarNoMenu: true
+  },
+  {
+    id: "quedacapilar",
+    nome: "Queda Capilar",
     mostrarNoMenu: true
   },
 
    {
-    id: "energia",
-    nome: "Energia",
+    id: "beleza",
+    nome: "Beleza",
     mostrarNoMenu: true
   }
 ];
@@ -45,48 +45,48 @@ const categorias = [
 const produtos = [
   {
     id: 1,
-    nome: "B-BURN DIA A DIA",
-    categoria: "energia",
+    nome: "Emagrecimento",
+    categoria: "emagrecimento",
     preco: 90.00,
     descricao: "Energia intensa, foco absoluto e performance contínua. Termogênese inteligente com disposição prolongada.",
-    imagem: "assets/img/BBURNDIADIAPOSTATIVA.jpg",
+    imagem: "assets/img/imgpoteteste.png",
     destaque: true
   },
   {
     id: 2,
-    nome: "Paracetamol 750mg",
-    categoria: "analgesicos",
+    nome: "Desempenho Físico",
+    categoria: "desempenhofisico",
     preco: 12.50,
     descricao: "Auxilia no tratamento de dores leves a moderadas.",
-    imagem: "assets/img/paracetamol.jpg",
+    imagem: "assets/img/imgpoteteste.png",
     destaque: false
   },
   {
     id: 3,
-    nome: "Protetor Solar FPS 50",
-    categoria: "cosmeticos",
+    nome: "Saúde",
+    categoria: "saude",
     preco: 49.90,
     descricao: "Proteção diária contra raios UVA e UVB.",
-    imagem: "assets/img/protetor-solar.jpg",
+    imagem: "assets/img/imgpoteteste.png",
     destaque: true
   },
   {
     id: 4,
-    nome: "Vitamina C 1000mg",
-    categoria: "vitaminas",
+    nome: "Queda Capilar",
+    categoria: "quedacapilar",
     preco: 29.90,
     descricao: "Contribui para a imunidade e ação antioxidante.",
-    imagem: "assets/img/vitamina-c.jpg",
+    imagem: "assets/img/imgpoteteste.png",
     destaque: true
   },
   {
     id: 5,
-    nome: "Sabonete Líquido Neutro",
-    categoria: "higiene",
+    nome: "beleza",
+    categoria: "Beleza",
     preco: 15.90,
     descricao: "Limpeza suave para todos os tipos de pele.",
-    imagem: "assets/img/sabonete.jpg",
-    destaque: false
+    imagem: "assets/img/imgpoteteste.png",
+    destaque: true
   }
 ];
 
